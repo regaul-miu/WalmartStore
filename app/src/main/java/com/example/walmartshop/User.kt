8a8043b13cf8214c9759c2779f609fe6318a72d3
regaul-miu <112953309+regaul-miu@users.noreply.github.com>
2022-10-12 13:36:57 -0500

    Update User.kt

--- app/src/main/java/com/example/walmartshop/User.kt
+++ app/src/main/java/com/example/walmartshop/User.kt
@@ -2,7 +2,7 @@ package com.example.walmartshop
 
 import java.io.Serializable
 
-class User(val userName:String, val password:String, val firstName:String,val lastName:String):
+data class User(val userName:String, val password:String, val firstName:String,val lastName:String):
     Serializable {
 
 
